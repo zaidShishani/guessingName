3894e276ce7c22e9316372739ba16520afa50a04
zaidShishani <zaidjamo94@Gmail.com>
2021-08-05 13:08:01 +0300

    html and css edits

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let userName = prompt("what is your name ??")
 
 let scorePoint = 0;
 
-//console.log('Hi ' + userName + " welcome to my website can you guess these right bout me :O")
+
 alert('Hi ' + userName + " welcome to my website can you guess these right bout me :O")
 let q1 = prompt("are dogs my favorite pets").toLowerCase()
 
@@ -14,17 +14,14 @@ switch (q1)
 {
    case "y":
    case "yes":
-    //console.log('Wow that\'s right ' + userName + "!!");
     alert('Wow that\'s right ' + userName + "!!");
     scorePoint++;
        break;
     case"n":
     case"no":
-    //console.log('That\'s not it my guy :\\ dogs are cool');
     alert('That\'s not it my guy :\\ dogs are cool');
        break;
    default: 
-   //console.log('did you even try :\\');
    alert('did you even try :\\');
    break;
 }
@@ -38,17 +35,14 @@ switch (q2)
 {
    case "y":
    case "yes":
-    //console.log('I think we can be friends');
     alert('I think we can be friends');
     scorePoint++;
        break;
     case"n":
     case"no":
-    //console.log('Nope no no no, ' + userName + ' you can do better');
     alert('Nope no no no, ' + userName + ' you can do better');
        break;
    default: 
-   //console.log('come on man...');
    alert('come on man...');
    break;
 }
@@ -61,17 +55,14 @@ switch (q3)
 {
    case "y":
    case "yes":
-    //console.log('I do but not as much as I used to');
     alert('I do but not as much as I used to');
     scorePoint++;
        break;
     case"n":
     case"no":
-    //console.log('wrong, I do play');
     alert('wrong, I do play');
        break;
    default: 
-   // console.log('you\'r input is incorrect try using y / n..')
    alert;('you\'r input is incorrect try using y / n..')
    break;
 }
@@ -84,17 +75,14 @@ switch (q4)
 {
    case "y":
    case "yes":
-    //console.log('true been hitting the gym been tryin to lose quarantine weight');
     alert('true been hitting the gym tryin to lose quarantine weight');
     scorePoint++;
        break;
     case"n":
     case"no":
-    //console.log('nope wish I had healthier eating habits');
     alert('nope wish I had healthier eating habits');
        break;
    default: 
-   //console.log('I give up....');
    alert('I give up....');
    break;
 }
@@ -107,17 +95,14 @@ switch (q5)
 {
    case "y":
    case "yes":
-    //console.log('worng, I think they are overrate');
     alert('worng, I think they are overrate');
        break;
     case"n":
     case"no":
-    //console.log('ohh you guess it, I think they are overrate');
     alert('ohh you guess it, I think they are overrate');
     scorePoint++;
        break;
    default: 
-   //console.log('try yes,no next time');
    alert('try yes,no next time');
    break;
 }
@@ -156,7 +141,6 @@ let q7attempts = 0
 while(q7attempts < 5){
  for (let i = 0; i < q7array.length; i++) {
       if(q7 === q7array[i]){
-         //console.log('that\'s correct')
          alert('that\'s correct')
          q7attempts = 5;
          scorePoint++;
@@ -168,7 +152,6 @@ while(q7attempts < 5){
    q7attempts++
    }
    if(q7attempts == 5){
-      //console.log('answers were (bojack horseman, avatar the Last Airbender, how I met your mother, the walking dead, the Umbrella Academy, the Witcher, the queens gambit, the good place, stranger things, lucifer)')
       alert('answers were (bojack horseman, avatar the Last Airbender, how I met your mother, the walking dead, the Umbrella Academy, the Witcher, the queens gambit, the good place, stranger things, lucifer)')
 
    }
